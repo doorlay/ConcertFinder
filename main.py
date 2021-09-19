6371de2e09c1c58a822ca7346f0985306b300524
doorlay <nic@doorlay.com>
2021-09-19 12:18:13 -0700

    Refactor Spotify algorithm, clean up code

--- main.py
+++ main.py
@@ -1,42 +1,41 @@
 import requests
 
-SPOTIFY_API_KEY = "BQBDFPH8h6gexAhfIXg2slqGshdgt2z5bomRuYdyA4alSGA74J8XYuaJ_RmTH36lQ-LHJxdGqccnyPEYq1WEbdeWFykgIg_w31gFzmUB9FBQ_YfKGc3cbTJ1gXquw1urICNZot-QHEJTwbOe0wk"
+SPOTIFY_API_KEY = "BQBkwLNZxvcjNPcsMD6zhSRe2mfWLGm2DBiReEErF7D3vuOz5v02fDZRX6JA-viy51vCHU5Tj2o4anvW285w2g6uMMJEqc_emKmKhkjNCZlsxx7U-EZ2stXPdqI49w1M_0vChy-SPJY3NpDnieM"
 TICKETMASTER_API_KEY = "qznGGwwSp4QK23CGqcXOOSB2DIHR2Zg4"
 
 
-def send_spotify_request(time_range, limit, api_key):
+def get_top_artists(time_range, limit, api_key):
         url = f"https://api.spotify.com/v1/me/top/artists?time_range={time_range}&limit={limit}"
         headers = {
                 "Accept": "application/json", 
                 "Content-Type": "application/json",
                 "Authorization": f"Bearer {api_key}"
                 }
-        return requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers)
+        return [entry["name"] for entry in response.json()["items"]]
 
 
-def extract_artists(api_response):
-        return [entry["name"] for entry in api_response.json()["items"]]
+def find_events(artist, api_key):
+        url = f"https://app.ticketmaster.com/discovery/v2/events.json?keyword={artist}&apikey={api_key}"
+        return requests.get(url).json()
 
 
-def get_top_artists(time_range, limit, api_key):
-        return extract_artists(send_spotify_request(time_range, limit, api_key))
+top_artists = get_top_artists("medium_term", 30, SPOTIFY_API_KEY)
+for artist in top_artists:
+        print(find_events(artist, TICKETMASTER_API_KEY))
 
-
-print(get_top_artists("medium_term", 30, SPOTIFY_API_KEY))
-
-
-def send_ticketmaster_request(api_key):
-        url = f"https://app.ticketmaster.com/discovery/v2/events.json?attractionId=K8vZ917Gku7&apikey={api_key}"
-        return requests.get(url)
-
-print(send_ticketmaster_request(TICKETMASTER_API_KEY))
 
 """
+
+Login to your Spotify and enter your zip code and ConcertFinder will find concerts near you for all of your top artists.
+
 
 The spotify api key expires every hour. Set up the whole workflow thing to get around this: https://developer.spotify.com/documentation/general/guides/authorization-guide/
 
 
 The TicketMaster Discovery API lets your search for events by artist.
+
+Join the TicketMaster affiliate program once I can get this going and this will be a great way to make money.
 
 Gets all events for Adele in Canada
 
